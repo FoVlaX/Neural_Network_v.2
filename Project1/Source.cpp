11cbeb8c1147f0d991be91e15721e371bdc1fb7a
FoVlaX <fovlaxcorp@gmail.com>
2020-02-01 13:16:40 +0300

    v.2

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -5,9 +5,11 @@
 #include <Magick++.h>
 using namespace std;
 using namespace Magick;
-double RandFunc() {
-	return double(rand() % 2456)/2457.0;
-}
+
+	double RandFunc() {
+		return double(rand() % 2456)/2457.0;
+	}
+
 
 int main(int argc, char** argv) {
 	InitializeMagick(*argv);
@@ -28,45 +30,41 @@ int main(int argc, char** argv) {
 		// now you can access single pixels like a vector
 		for (int row = 0; row < h; row++) {
 			for (int column = 0; column < w; column++) {
-				digits[i][w*row+column][0] = double(65535 - pixels[w * row + column])/65535.0;
+				digits[i][w*row+column][0] = double(65535-pixels[w * row + column])/65535.0;
 			}
 		}
 	}
 	
-	//NN network(1,1000,6400,10);
-	NN network("test.txt");
-	matrix<double> a0(vector<vector<double>> { {1.0}, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0}, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 } });
-	matrix<double> a1(vector<vector<double>>{ {0.0}, { 1.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 } });
-	matrix<double> a2(vector<vector<double>>{ {0.0}, { 0.0 }, { 1.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 } });
-	matrix<double> a3(vector<vector<double>>{ {0.0}, { 0.0 }, { 0.0 }, { 1.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 } });
-	matrix<double> a4(vector<vector<double>>{ {0.0}, { 0.0 }, { 0.0 }, { 0.0 }, { 1.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 } });
-	matrix<double> a5(vector<vector<double>>{ {0.0}, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 1.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 } });
-	matrix<double> a6(vector<vector<double>>{ {0.0}, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 1.0 }, { 0.0 }, { 0.0 }, { 0.0 } });
-	matrix<double> a7(vector<vector<double>>{ {0.0}, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 1.0 }, { 0.0 }, { 0.0 } });
-	matrix<double> a8(vector<vector<double>>{ {0.0}, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 1.0 }, { 0.0 } });
-	matrix<double> a9(vector<vector<double>>{ {0.0}, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 0.0 }, { 1.0 } });
+	NN network(1,1000,6400,10);
+	//NN network("test.txt");
+
+	vector<matrix<double>> a(10);
+	for (int i = 0; i < 10; i++) {
+		a[i].change(10, 1);
+		a[i][i][0] = 1;
+	}
+
 	const int count = 300;
-	/*for (int i = 0; i < count; i++){
-		network.teach(digits[0], a0);
-		network.teach(digits[1], a1);
-		network.teach(digits[2], a2);
-		network.teach(digits[3], a3);
-		network.teach(digits[4], a4);
-		network.teach(digits[5], a5);
-		network.teach(digits[6], a6);
-		network.teach(digits[7], a7);
-		network.teach(digits[8], a8);
-		network.teach(digits[9], a9);
+	for (int i = 0; i < count; i++){
+		network.teach(digits[0], a[0]);
+		network.teach(digits[1], a[1]);
+		network.teach(digits[2], a[2]);
+		network.teach(digits[3], a[3]);
+		network.teach(digits[4], a[4]);
+		network.teach(digits[5], a[5]);
+		network.teach(digits[6], a[6]);
+		network.teach(digits[7], a[7]);
+		network.teach(digits[8], a[8]);
+		network.teach(digits[9], a[9]);
 		if (i % 30) {
 			system("cls");
 			cout << (double(i) / count * 100);
 		}
-	}*/
+	}
 	cout.precision(3);
 	cout <<endl<< fixed << network.feedfoward(digits[0]).ans << endl;
-	cout << network.feedfoward(digits[1]).ans<< endl;
-	cout << network.feedfoward(digits[4]).ans.MaxIndex().first << endl;
+	cout << network.feedfoward(digits[2]).ans<< endl;
+	cout << network.feedfoward(digits[8]).ans << endl;
 	network.save("test.txt");
-	
 	return 0;
 }
